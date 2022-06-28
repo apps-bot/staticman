960e329f085bb20ed7a61da6fea73af442026836
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2022-06-28 23:23:44 +0200

    Update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -2,6 +2,10 @@
 
 const pkg = require('./../package.json')
 
+app.get('/cervezas', function(req, res) {
+  res.json({ mensaje: '¡A beber cerveza!' })  
+})
+
 module.exports = (req, res) => {
   res.send(`StaticMan v ${pkg.version}!`)
 }
