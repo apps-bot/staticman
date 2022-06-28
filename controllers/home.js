b23c70fb4d714965ae700cc3c784ded138fe93ab
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2022-06-28 23:38:52 +0200

    Update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -2,10 +2,6 @@
 
 const pkg = require('./../package.json')
 
-app.get('/error', (req, res) => {
-  res.send('Custom error landing page')
-}
-
 module.exports = (req, res) => {
   res.send(`StaticMan v ${pkg.version}!`)
 }
