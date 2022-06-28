a9b287316b1b15091e4c2b3a699ada4e10250a2d
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2022-06-28 20:22:43 +0200

    Update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,9 @@
 'use strict'
 
-app.use('/', express.static(__dirname + '/public'))
+const express = require("express");
+const app = express();
+
+app.use("/", express.static(__dirname + "/public"));
 
 const pkg = require('./../package.json')
 
