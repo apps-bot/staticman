eb799484a9a0b96d9cf3e911f51f0e4edc349663
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2022-06-28 22:40:41 +0200

    Update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,11 @@
 'use strict'
 
+var app = express();
+app.get('/help', function(req, res) {
+    res.sendFile('public/help.html', {root: __dirname })
+});
+
+
 const pkg = require('./../package.json')
 
 module.exports = (req, res) => {
