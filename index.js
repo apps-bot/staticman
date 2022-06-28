d17802e183dbdf4a6e30a57cc1e48a383c75d4e0
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2022-06-28 22:26:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+var express = require('../..');
+var path = require('path');
+var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 const StaticmanAPI = require('./server')
